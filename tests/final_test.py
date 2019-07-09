be220e0f9779ccafc1b0cf36a2ace68ab8c7d633
alex2018hillel <alex2018hillel@gmail.com>
2019-07-09 14:45:00 +0300

    Initial commit

---
+++ tests/final_test.py
@@ -0,0 +1,55 @@
+import unittest
+import configparser
+from utils.login_page import Login_page
+from utils.create_page import Create_mail
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+class final_test(unittest.TestCase):
+    driver = None
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        # self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        self.driver.implicitly_wait(10)
+
+    def test_first(self, id = "ID_115"):
+        wdriver = self.driver
+        parser = configparser.ConfigParser()
+        parser.read('simple_config.ini')
+        url = parser.get('data', 'url')
+        user_name = parser.get('data', 'username')
+        password = parser.get('data', 'password')
+
+        # Login to mailbox form
+        wdriver.get(url)
+        login_field = wdriver.find_element_by_xpath(Login_page.login_field)
+        login_field.send_keys(user_name)
+        password_field = wdriver.find_element_by_xpath(Login_page.password_field)
+        password_field.send_keys(password)
+        button_login = wdriver.find_element_by_xpath(Login_page.button_login)
+        button_login.click()
+        wdriver.implicitly_wait(10)
+        user_mail = wdriver.find_element_by_xpath(Login_page.user_mail)
+        print(user_mail.text)
+        assert user_mail.text == Create_mail.expected_name
+
+        # Create e-mail
+        wdriver.find_element_by_xpath(Create_mail.create_button).click()
+        wdriver.find_element_by_xpath(Create_mail.fild_input).send_keys(Create_mail.expected_name)
+        wdriver.find_element_by_xpath(Create_mail.fild_subject).send_keys(id)
+        wdriver.find_element_by_xpath(Create_mail.submit_button).click()
+        wdriver.find_element_by_xpath("//a[@id='0']/span[4]").click()
+        wdriver.implicitly_wait(10)
+
+        # Find e-mail
+        elem = wdriver.find_elements_by_xpath("//*[text()='ID_115']")[0]
+        print(elem.text)
+        assert elem.text == id
+
+    def tear_down(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+        unittest.main()
---
+++ utils/create_page.py
@@ -0,0 +1,8 @@
+
+class Create_mail:
+    expected_name = "alex2019hillel@ukr.net"
+
+    create_button = "//div[@id='content']/aside/button"
+    fild_input = "//input[@name='toFieldInput']"
+    fild_subject = "//input[@name='subject']"
+    submit_button = "//div[@id='screens']/div/div/div/button"
