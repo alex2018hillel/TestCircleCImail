cd9976d6ecf09d6e494e900e04a09a00bc344dbd
alex2018hillel <alex2018hillel@gmail.co>
2019-07-09 18:39:24 +0300

    add utils

---
+++ tests/utils/login_page.py
@@ -0,0 +1,14 @@
+
+class Login_page:
+
+    login_field = "//*[@id='id-l']"
+    password_field = "//*[@id='id-p']"
+    button_login = "//*[@type='submit']"
+    user_mail = "//*[@class='login-button__user']"
+
+
+
+
+
+
+
