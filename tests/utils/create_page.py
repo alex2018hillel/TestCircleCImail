79ee6b40fed6c3b0482cdc1cf009dcf338d4cfab
alex2018hillel <alex2018hillel@gmail.co>
2019-07-14 00:51:43 +0300

    create new assert & locator

--- tests/utils/create_page.py
+++ tests/utils/create_page.py
@@ -2,7 +2,8 @@
 class Create_mail:
     expected_name = "alex2019hillel@ukr.net"
 
-    create_button = "//div[@id='content']/aside/button"
+    #create_button = "//div[@id='content']/aside/button"
+    create_button = "//button[@class='default compose']"
     fild_input = "//input[@name='toFieldInput']"
     fild_subject = "//input[@name='subject']"
     submit_button = "//div[@id='screens']/div/div/div/button"
