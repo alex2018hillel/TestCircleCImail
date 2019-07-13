e69ceda27bd1aacb66d8f9c936601e5ceb0f2af7
alex2018hillel <alex2018hillel@gmail.co>
2019-07-13 13:40:25 +0300

    add locators

--- tests/utils/login_page.py
+++ tests/utils/login_page.py
@@ -4,7 +4,8 @@ class Login_page:
     login_field = "//*[@id='id-l']"
     password_field = "//*[@id='id-p']"
     button_login = "//*[@type='submit']"
-    user_mail = "//*[@id='id-user-email']"
+    #user_mail = "//*[@id='id-user-email']"
+    user_mail = "//*[@class='login-button_user']"
 
 
 
